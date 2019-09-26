19265c9872f1bc9be120b35ff92c6cbff819c180
Ajitha <ajitha.buvanachandran@gmail.com>
2019-09-26 13:51:54 -0400

    added new range functionality by adding getting parameters from terminal

--- generator.py
+++ generator.py
@@ -1,6 +1,9 @@
-input_number1 = input("Enter start of range ")
+import sys
+
+print('Argument List:', str(sys.argv))
+input_number1 = sys.argv[1]
 if input_number1.isdigit():
-    input_number2 = input("Enter end of range ")
+    input_number2 = sys.argv[2]
     if input_number2.isdigit():
         print(list(range(int(input_number1), int(input_number2))))
     else:
