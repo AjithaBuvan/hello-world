90a4042924c9f29b5ef1bb326a89c2630f2de0de
Ajitha <ajitha.buvanachandran@gmail.com>
2019-09-26 13:34:12 -0400

    added new range functionality

--- generator.py
+++ generator.py
@@ -1,2 +1,9 @@
-print('This is a generator example')
-print('testing git')
+input_number1 = input("Enter start of range ")
+if input_number1.isdigit():
+    input_number2 = input("Enter end of range ")
+    if input_number2.isdigit():
+        print(list(range(int(input_number1), int(input_number2))))
+    else:
+        print("Please enter valid input")
+else:
+    print("Please enter valid input")
